ee7b92babe37a5370a3d50eb282660831f43316a
jad1223 <100876658+jad1223@users.noreply.github.com>
2022-03-10 09:51:54 -0500

    Update FastCollinearPoints.java

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -1,25 +1,31 @@
 import java.util.*;
 public class FastCollinearPoints {
     private int numberOfSegments;
+    private ArrayList<LineSegment> lineSegments;
     public FastCollinearPoints(Point[] points){
-        double[] slopes;
+        Point[] copy;
+        lineSegments = new ArrayList<LineSegment>();
         for(int i = 0; i<points.length-1; i++){
             Point p = points[i];
             Comparator<Point> c = p.slopeOrder();
-            Arrays.sort(points, c);
-            slopes = new double[points.length-i];
-            slopes[i] = p.slopeTo(points[i]);
-            for(int j = i+1; j<points.length; j++){
-                double currentSlope = slopes[j];
-               
-                }
+            copy = points;
+            Arrays.sort(copy, c);
+            for(int j = i+1; j<points.length-3; j++){
+                    double slopePlusOne = p.slopeTo(copy[j+1]);
+                    double slopePlusTwo = p.slopeTo(copy[j+2]);
+                    double slopePlusThree = p.slopeTo(copy[j+3]);
+                    if(slopePlusOne == slopePlusTwo &&
+                            slopePlusTwo == slopePlusThree){
+                        numberOfSegments++;
+                        lineSegments.add(new LineSegment (p, copy[j+3]));
+                    }
             }
         }
     }
     public int numberOfSegments(){
         return numberOfSegments;
     }
-    public LineSegment[] segments(){
-        return null;
+    public ArrayList<LineSegment> segments(){
+        return lineSegments;
     }
 }
