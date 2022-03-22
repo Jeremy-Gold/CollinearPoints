08ed514554a643a7413fa3aca612751e2a7f2d15
jad1223 <100876658+jad1223@users.noreply.github.com>
2022-03-21 20:50:18 -0400

    Update FastCollinearPoints.java

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -1,27 +1,33 @@
-package goodPackage;
-
 import java.util.*;
 public class FastCollinearPoints {
     private int numberOfSegments;
     private ArrayList<LineSegment> lineSegments;
     public FastCollinearPoints(Point[] points){
         Point[] copy;
+        LineSegment[] used;
         lineSegments = new ArrayList<LineSegment>();
         for(int i = 0; i<points.length; i++){
             Point p = points[i];
             Comparator<Point> c = p.slopeOrder();
             copy = Arrays.copyOf(points, points.length);
             Arrays.sort(copy, c);
-            for(int j = 0; j<points.length-2; j++){
-                    double slopeOne = p.slopeTo(copy[j+1]);
-                    double slopeTwo = p.slopeTo(copy[j+2]);
-                    double slopeThree = p.slopeTo(copy[j+3]);
-                    
-                    //TODO Fix this
+            for(int j = 1; j<points.length-2; j++){
+                boolean alreadyThere = false;
+                    double slopeOne = p.slopeTo(copy[j]);
+                    double slopeTwo = p.slopeTo(copy[j+1]);
+                    double slopeThree = p.slopeTo(copy[j+2]);
                     if(slopeOne == slopeTwo &&
                             slopeTwo == slopeThree){
-                        numberOfSegments++;
-                        lineSegments.add(new LineSegment (p, copy[j+3]));
+                        LineSegment seg = new LineSegment (firstPoint(p, copy[j], copy[j+1], copy[j+2]), lastPoint(p, copy[j], copy[j+1], copy[j+2]));
+                        for(LineSegment s : lineSegments){
+                            if(s.equals(seg)){
+                                alreadyThere = true;
+                            }
+                        }
+                        if(!alreadyThere){
+                            lineSegments.add(seg);
+                            numberOfSegments++;
+                        }
                     }
             }
         }
@@ -31,5 +37,35 @@ public class FastCollinearPoints {
     }
     public ArrayList<LineSegment> segments(){
         return lineSegments;
+    }
+    public Point firstPoint(Point a, Point b, Point c, Point d){
+        if(a.compareTo(b)<0 && a.compareTo(c)<0 && a.compareTo(d)<0){
+            return a;
+        }
+        if(b.compareTo(a)<0 && b.compareTo(c)<0 && b.compareTo(d)<0){
+            return b;
+        }
+        if(c.compareTo(a)<0 && c.compareTo(b)<0 && c.compareTo(d)<0){
+            return c;
+        }
+        if(d.compareTo(a)<0 && d.compareTo(b)<0 && d.compareTo(c)<0){
+            return d;
+        }
+        return a;
+    }
+    public Point lastPoint(Point a, Point b, Point c, Point d){
+        if(a.compareTo(b)>0 && a.compareTo(c)>0 && a.compareTo(d)>0){
+            return a;
+        }
+        if(b.compareTo(a)>0 && b.compareTo(c)>0 && b.compareTo(d)>0){
+            return b;
+        }
+        if(c.compareTo(a)>0 && c.compareTo(b)>0 && c.compareTo(d)>0){
+            return c;
+        }
+        if(d.compareTo(a)>0 && d.compareTo(b)>0 && d.compareTo(c)>0){
+            return d;
+        }
+        return a;
     }
 }
